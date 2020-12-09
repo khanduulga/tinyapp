6b2163710e5e2868f11ecdfa7ef78bb4ad38c9fc
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-12-09 06:31:22 +0000

    Created a URL submission form and installed body parser to read the body of POST requests that come from the form's submit

--- express_server.js
+++ express_server.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
+
+const generateRandomString = () => {
+  let result = "";
+  const possibleChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const possibleCharLength = possibleChar.length;
+  for (i = 0; i < 6; i++) {
+    result += possibleChar[Math.floor(Math.random() * charactersLength)];
+  }
+  return result;
+}
+
+//middware
+app.use(bodyParser.urlencoded({extended: true}));
 
 //template engine setup
 app.set("view engine", "ejs");
@@ -31,10 +45,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+//POST new URL request
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
